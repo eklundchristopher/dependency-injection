3c3368c99e51574027f2c7f0125f6eec1d890d64
Christopher <eklundchristopher@gmail.com>
2014-11-26 23:53:00 +0100

    Minor adjustments

--- custom.php
+++ custom.php
@@ -1,9 +1,9 @@
 <?php
 
 // Override any existing container instances here.
-//   app()->bind('dog', 'Custom\Dog');
+//   app()->bind('dog', 'Custom\Repositories\StrayDog');
 
-// Create a dog, make it walk for a little bit and than have it
+// Create a dog, make it walk for a little bit and then have it
 // eat on a snack of your choice.
 $dog = app()->make('dog');
 
