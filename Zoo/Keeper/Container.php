0f068bdd939c9a96ef4afa681dd62cb081346762
Christopher Eklund <eklundchristopher@gmail.com>
2015-01-31 13:36:15 +0100

    Documentation fix

--- Zoo/Keeper/Container.php
+++ Zoo/Keeper/Container.php
@@ -29,7 +29,7 @@ class Container {
 	/**
 	 * Retrieve the Container object.
 	 *
-	 * @return Zoo\Keeper\Container|null
+	 * @return \Zoo\Keeper\Container|null
 	 */
 	public static function getInstance()
 	{
